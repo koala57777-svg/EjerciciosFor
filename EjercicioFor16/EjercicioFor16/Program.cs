97a4d27dddb7b44a61fe15e7477b69e5bfd6c83d
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 17:32:52 -0600

    Proyecto terminado 1.1 : Se arregló luego del "descubrimiento" del ejercicio 20.

--- EjercicioFor16/EjercicioFor16/Program.cs
+++ EjercicioFor16/EjercicioFor16/Program.cs
@@ -1,20 +1,15 @@
 ﻿int numero=0;
-
+int cantidad = 0;
 for (int i=1; i<=5; i++)
 {
     Console.WriteLine($"Ingrese su {i}° número: ");
     numero=int.Parse( Console.ReadLine() );
- 
-}
-
-int cantidad = 0;
-for (int j=1; j<=5; j++)
-{
-    if (numero>10)
+    if (numero > 10)
     {
-        cantidad = j;
+        cantidad = cantidad + 1;
     }
+
 }
 
-Console.WriteLine($"Cantidad que son mayores a 10 son: {cantidad}");
+Console.WriteLine($"Cantidad de números que son mayores a 10 son: {cantidad}");
 

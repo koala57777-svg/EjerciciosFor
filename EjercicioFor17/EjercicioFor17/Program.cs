0da007c9b02c8efe14053d5f463739eae31c340c
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 17:35:13 -0600

    Proyecto terminado 1.1 : Se arreglo debio a conocimineto obtenido del ejercicio 20.

--- EjercicioFor17/EjercicioFor17/Program.cs
+++ EjercicioFor17/EjercicioFor17/Program.cs
@@ -1,17 +1,13 @@
 ﻿int numerosingresados = 0;
-
+int cantidadpares = 0;
 for (int i=1; i<=5; i++)
 {
     Console.WriteLine($"Ingrese su {i}° número: ");
     numerosingresados =int.Parse( Console.ReadLine() );
-}
-
-int cantidadpares = 0;
-for (int j=1; j<=5; j++)
-{
+    
     if (numerosingresados % 2 == 0)
     {
-        cantidadpares = j;
+        cantidadpares = cantidadpares + 1;
     }
 }
 

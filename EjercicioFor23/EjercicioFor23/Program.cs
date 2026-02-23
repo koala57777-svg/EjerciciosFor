0929aa92c77919b4fe7297cfff863b5fa067c9ac
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 17:50:52 -0600

    Se terminó el proyecto 23.

--- EjercicioFor23/EjercicioFor23/Program.cs
+++ EjercicioFor23/EjercicioFor23/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Ponga la cantida de notas que necesita ingresar: ");
+int cantidadNotas = int.Parse(Console.ReadLine());
+
+double totalNotas = 0;
+double Nota = 0;
+int contador = 1;
+for (int i = 1; i <= cantidadNotas; i++)
+{
+    Console.WriteLine($"Ingrese la {i}° nota: ");
+    Nota = double.Parse(Console.ReadLine());
+    totalNotas = totalNotas + Nota;
+    contador = i;
+}
+
+Console.WriteLine($"El promedio de las notas es: {totalNotas/contador}");

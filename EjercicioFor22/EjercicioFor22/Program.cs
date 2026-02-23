d4d44f48a5b77f06f465a65f0ca90a780b7cdae3
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 17:45:12 -0600

    Se terminó el proyecto 22.

--- EjercicioFor22/EjercicioFor22/Program.cs
+++ EjercicioFor22/EjercicioFor22/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Ponga la cantida de ventas que necesita ingresar: ");
+int cantidadVentas = int.Parse(Console.ReadLine());
+
+double totalVentas = 0;
+double venta = 0;
+
+for(int i=1; i<=cantidadVentas; i++)
+{
+    Console.WriteLine($"Ingrese el monto de la {i}° venta (Q): ");
+    venta = double.Parse(Console.ReadLine());
+    totalVentas = totalVentas + venta;
+}
+
+Console.WriteLine($"El total de ventas fue: Q {totalVentas}");

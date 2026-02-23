3cc781d495088605c4df6893ff992d6881b20a9e
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 16:06:35 -0600

    Se terminó el proyecto 15.

--- EjercicioFor15/EjercicioFor15/Program.cs
+++ EjercicioFor15/EjercicioFor15/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int numero = 0;
+int numerostotales = 0;
+for (int i=1; i<=5; i++)
+{
+    Console.WriteLine($"Ingrese su {i}° número: ");
+    numero=int.Parse(Console.ReadLine()) + numero;
+    numerostotales = i;
+}
+Console.WriteLine($"El promedio es: {numero/numerostotales}");

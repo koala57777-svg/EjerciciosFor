b031f91e12aa74997b75842e1ab633f3fb87b8b9
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 17:38:34 -0600

    Se terminó el proyecto 21.

--- EjercicioFor21/EjercicioFor21/Program.cs
+++ EjercicioFor21/EjercicioFor21/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.WriteLine("Ingrese la cantidad de números que quiere ingresar: ");
+int cantidaddenumeros = int.Parse(Console.ReadLine());
+int numerosingresados = 0;
+int cuantosnegativos = 0;
+
+for (int i = 1; i <= cantidaddenumeros; i++)
+{
+    Console.WriteLine($"Ingrese su {i}° número: ");
+    numerosingresados = int.Parse(Console.ReadLine());
+
+    if (numerosingresados < 0)
+    {
+        cuantosnegativos = cuantosnegativos + 1;
+    }
+
+}
+
+
+Console.WriteLine($"La cantidad de números que son negativos son: {cuantosnegativos}");

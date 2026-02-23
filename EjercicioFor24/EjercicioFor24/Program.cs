a2f25c2271766cc3e48202b132c60ce49eeb5f96
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 17:55:37 -0600

    Se terminó el proyecto 24.

--- EjercicioFor24/EjercicioFor24/Program.cs
+++ EjercicioFor24/EjercicioFor24/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Console.WriteLine("Ponga la cantida de notas que necesita ingresar: ");
+int cantidadNotas = int.Parse(Console.ReadLine());
+
+double totalNotas = 0;
+double Nota = 0;
+int contador = 1;
+for (int i = 1; i <= cantidadNotas; i++)
+{
+    Console.WriteLine($"Ingrese la {i}° nota: ");
+    Nota = double.Parse(Console.ReadLine());
+    totalNotas = totalNotas + Nota;
+    contador = i;
+}
+
+if ((totalNotas / contador) >= 61)
+{
+    Console.WriteLine($"El promedio es: {totalNotas / contador} y el alumno esta aprobado");
+}
+else
+{
+    Console.WriteLine($"El promedio es: {totalNotas / contador} y el alumno esta reprobado");
+}

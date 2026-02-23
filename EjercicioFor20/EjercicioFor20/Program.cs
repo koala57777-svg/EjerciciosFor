e7fce03b48a5532f27b0a6c82ba770a9483b749a
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 17:29:26 -0600

    Se terminó el proyecto 20. Se aprendió a como hacer un contador (hasta ahora xd).

--- EjercicioFor20/EjercicioFor20/Program.cs
+++ EjercicioFor20/EjercicioFor20/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.WriteLine("Ingrese la cantidad de números que quiere ingresar: ");
+int cantidadnumeros = int.Parse(Console.ReadLine());
+int numerosingresados = 0;
+int cuantospositivos = 0;
+
+for (int i=1; i<=cantidadnumeros; i++)
+{
+    Console.WriteLine($"Ingrese su {i}° número: ");
+    numerosingresados= int.Parse(Console.ReadLine());
+
+    if (numerosingresados > 0)
+    {
+        cuantospositivos=cuantospositivos+1;
+    }
+    
+}
+
+
+Console.WriteLine($"La cantidad de números que son positivos son: {cuantospositivos}");

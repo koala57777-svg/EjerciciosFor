47f60eafd7a918660c645381cb5fa8bbeb8d330b
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 16:00:45 -0600

    Se terminó el proyecto 14. Aprendí que se puede automatizar mucho el pedir un número.

--- EjercicioFor14/EjercicioFor14/Program.cs
+++ EjercicioFor14/EjercicioFor14/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿int numero = 0;
+for (int i=1; i<=3; i++)
+{
+    Console.WriteLine($"Ingrese su {i}° número: ");
+    numero =  int.Parse( Console.ReadLine()) + numero;
+}
+
+Console.WriteLine($"La suma de los números es: {numero}");

67e4665a0783b98bd13dbef279703efe63dfde97
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 17:15:39 -0600

    Se terminó el proyecto 19.

--- EjercicioFor19/EjercicioFor19/Program.cs
+++ EjercicioFor19/EjercicioFor19/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.Timers;
+
+int numerosingresados = 0;
+int numeromenor = int.MaxValue;
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Ingrese su {i}° número: ");
+    numerosingresados = int.Parse(Console.ReadLine());
+    if (numerosingresados < numeromenor)
+    {
+        numeromenor = numerosingresados;
+    }
+
+}
+
+Console.WriteLine($"El número mayor es {numeromenor}");

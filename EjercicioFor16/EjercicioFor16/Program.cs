7855b49b6d3fc1f38cf04e3ed55cf0a57496efa9
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 16:31:38 -0600

    Se terminó el proyecto 16.

--- EjercicioFor16/EjercicioFor16/Program.cs
+++ EjercicioFor16/EjercicioFor16/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int numero=0;
+
+for (int i=1; i<=5; i++)
+{
+    Console.WriteLine($"Ingrese su {i}° número: ");
+    numero=int.Parse( Console.ReadLine() );
+ 
+}
+
+int cantidad = 0;
+for (int j=1; j<=5; j++)
+{
+    if (numero>10)
+    {
+        cantidad = j;
+    }
+}
+
+Console.WriteLine($"Cantidad que son mayores a 10 son: {cantidad}");
+

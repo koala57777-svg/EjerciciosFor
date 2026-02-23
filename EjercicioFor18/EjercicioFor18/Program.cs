40c37069af912962aa0d08fbf61cb8b4de4a2c7a
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 17:11:45 -0600

    Se terminó el proyecto 18. Investigue su resolución.

--- EjercicioFor18/EjercicioFor18/Program.cs
+++ EjercicioFor18/EjercicioFor18/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.Timers;
+
+int numerosingresados = 0 ;
+int numeromayor = int.MinValue;
+for (int i=1; i<=5; i++)
+{
+    Console.WriteLine($"Ingrese su {i}° número: ");
+    numerosingresados =int.Parse(Console.ReadLine());
+   if (numerosingresados> numeromayor)
+    {
+        numeromayor = numerosingresados;
+    }
+
+}
+
+Console.WriteLine($"El número mayor es {numeromayor}");

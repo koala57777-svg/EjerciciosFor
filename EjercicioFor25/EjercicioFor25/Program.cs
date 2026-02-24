db5d0852b683acd10fd38d0f02c3456dcdb3c15e
Gerber Ramírez <koala57777@gmail.com>
2026-02-23 18:14:13 -0600

    Se temrinó el proyecto 25.

--- EjercicioFor25/EjercicioFor25/Program.cs
+++ EjercicioFor25/EjercicioFor25/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Ingrese un número: ");
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+string salida = "";
+
+for (int i=1; i<= numero; i++)
+{
+    salida = salida + "*";
+    Console.WriteLine(salida);
+}
+
